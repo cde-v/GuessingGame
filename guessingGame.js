1fecbb65e0c0fd3b2ad7ad49ccaf47bd359ad498
Chris de Villiers <cpdevill@gmail.com>
2016-01-30 21:43:48 -0500

    Foundations workshop complete, not refactored yet.

--- guessingGame.js
+++ guessingGame.js
@@ -9,105 +9,105 @@ var guessListArr = [];
 /* **** Guessing Game Functions **** */
 
 // Generate the Winning Number
-function generateWinningNumber(){
-	return Math.ceil(Math.random()*100);
+function generateWinningNumber() {
+  return Math.ceil(Math.random() * 100);
 }
 
 // Fetch the Players Guess
 function playersGuessSubmission() {
-	var duplicateGuess = false;
+  var duplicateGuess = false;
 
-	var playersGuessFloat = parseFloat($("#playersGuess").val());
-	playersGuess = parseInt($("#playersGuess").val(), 10);
+  var playersGuessFloat = parseFloat($("#playersGuess").val());
+  playersGuess = parseInt($("#playersGuess").val(), 10);
 
-	for(var i = 0; i < guessListArr.length; i++) {
-		if(guessListArr[i] === playersGuess) duplicateGuess = true;
-	}
-	
-	$("#playersGuess").val("");
+  for (var i = 0; i < guessListArr.length; i++) {
+    if (guessListArr[i] === playersGuess) duplicateGuess = true;
+  }
 
-	if( isNaN(playersGuess) || playersGuess < 1 || playersGuess > 100) {
-		$("#status").html("<span style='color:red'><b>Error: </b></span>Please enter a valid target distance (1-100).");
-	} else if(playersGuess != playersGuessFloat) {
-		$("#status").html("<span style='color:red'><b>Error: </b></span>Targeting system only accepts <b>whole numbers</b>.");
-	} else if(duplicateGuess) {
-		$("#status").html("<span style='color:red'><b>Error: </b></span>Previously targeted. Enter a new target distance.");		
-	} else {
-		guessListArr.push(playersGuess);
-		guessesRemaining--;
-		checkGuess();
-		$("#prevGuesses").append("<h5>" + playersGuess +" " + "</h5>");
-	}
+  $("#playersGuess").val("");
+
+  if (isNaN(playersGuess) || playersGuess < 1 || playersGuess > 100) {
+    $("#status").html("<span style='color:red'><b>Error: </b></span>Please enter a valid target distance (1-100).");
+  } else if (playersGuess != playersGuessFloat) {
+    $("#status").html("<span style='color:red'><b>Error: </b></span>Targeting system only accepts <b>whole numbers</b>.");
+  } else if (duplicateGuess) {
+    $("#status").html("<span style='color:red'><b>Error: </b></span>Previously targeted. Enter a new target distance.");
+  } else {
+    guessListArr.push(playersGuess);
+    guessesRemaining--;
+    checkGuess();
+    $("#prevGuesses").append("<h5>" + playersGuess + " " + "</h5>");
+  }
 }
 
 // Determine if the next guess should be a lower or higher number
-function lowerOrHigher(){
-	return playersGuess - winningNumber;
-	// add code here
+function lowerOrHigher() {
+  return playersGuess - winningNumber;
+  // add code here
 }
 
 // Check if the Player's Guess is the winning number 
-function checkGuess(){
-	if(playersGuess === winningNumber) onWin();
-	else if(guessesRemaining === 0) onLose();
-	else onTryAgain();
+function checkGuess() {
+  if (playersGuess === winningNumber) onWin();
+  else if (guessesRemaining === 0) onLose();
+  else onTryAgain();
 }
 
 // Create a provide hint button that provides additional clues to the "Player"
-function provideHint(){
-	$("#status").text( (winningNumber%2===0) ? "The enemy ship is an even number of km away." : "The enemy ship is an odd number of km away." );
+function provideHint() {
+  $("#status").text((winningNumber % 2 === 0) ? "The enemy ship is an even number of km away." : "The enemy ship is an odd number of km away.");
 }
 
 // Allow the "Player" to Play Again
-function playAgain(){
-	enableGameStuff();
-	winningNumber = generateWinningNumber();
-	guessesRemaining = 7;
-	guessListArr = [];
-	updateAttemptsRem();
-	$("#status").text("");
-	$("h5").remove();
+function playAgain() {
+  enableGameStuff();
+  winningNumber = generateWinningNumber();
+  guessesRemaining = 7;
+  guessListArr = [];
+  updateAttemptsRem();
+  $("#status").text("");
+  $("h5").remove();
 }
 
 // When the player wins
 function onWin() {
-	$("#status").text("You hit and sunk their Battleship!");
-	disableGameStuff();
+  $("#status").text("You hit and sunk their Battleship!");
+  disableGameStuff();
 }
 
 // When the player misses but still has attempts give them directional/proximity info.
 function onTryAgain() {
-	var diff = lowerOrHigher();
-	var direction = (diff > 0 ? "overshot" : "undershot");
-	var distanceEst = Math.abs(diff) + (10 - (Math.abs(diff)%10));
-	$("#status").text("You " + direction + " your target by less than " + distanceEst +"km.");
-	updateAttemptsRem();
+  var diff = lowerOrHigher();
+  var direction = (diff > 0 ? "overshot" : "undershot");
+  var distanceEst = Math.abs(diff) + (10 - (Math.abs(diff) % 10));
+  $("#status").text("You " + direction + " your target by less than " + distanceEst + "km.");
+  updateAttemptsRem();
 }
 
 // When the player misses with no attempts left
 function onLose() {
-	$("#status").text("You lose, evil wins. Try again.");
-	disableGameStuff();
+  $("#status").text("You lose, evil wins. Try again.");
+  disableGameStuff();
 }
 
 //Update the number of attempts remaining message
 function updateAttemptsRem() {
-	$("#attemptsRem").text(guessesRemaining + " attempts left.");
+  $("#attemptsRem").text(guessesRemaining + " attempts left.");
 }
 
 //Disable some buttons and input field after game is over, before restarting.
 function disableGameStuff() {
-	$("#submitGuess").prop("disabled", true).removeClass("btn-success").addClass("btn-default");
-	$("#hint").prop("disabled", true).removeClass("btn-info").addClass("btn-default");
-	$("#playersGuess").prop("disabled", true);
-	$("#attemptsRem").text("");
+  $("#submitGuess").prop("disabled", true).removeClass("btn-success").addClass("btn-default");
+  $("#hint").prop("disabled", true).removeClass("btn-info").addClass("btn-default");
+  $("#playersGuess").prop("disabled", true);
+  $("#attemptsRem").text("");
 }
 
 //Enable buttons and input field
 function enableGameStuff() {
-	$("#submitGuess").prop("disabled", false).removeClass("btn-default").addClass("btn-success");
-	$("#hint").prop("disabled", false).removeClass("btn-default").addClass("btn-info");
-	$("#playersGuess").prop("disabled", false);
+  $("#submitGuess").prop("disabled", false).removeClass("btn-default").addClass("btn-success");
+  $("#hint").prop("disabled", false).removeClass("btn-default").addClass("btn-info");
+  $("#playersGuess").prop("disabled", false);
 }
 
 $("#buttons").append("<div id='prevGuesses'><h2>Previous Guesses:</h2></div>");
@@ -120,7 +120,7 @@ $("#submitGuess").on("click", playersGuessSubmission);
 
 //Enter button
 $("#playersGuess").on("keypress", function(event) {
-	if(event.which == 13) playersGuessSubmission();
+  if (event.which == 13) playersGuessSubmission();
 });
 
 //Hint button
@@ -128,9 +128,3 @@ $("#hint").on("click", provideHint);
 
 //restart button
 $("#restartSim").on("click", playAgain);
-
-
-
-
-
-
